30ba3b456f43af938e1f36866a95eff8f27a97c1
Dan <Daniel-J-Price.dev@pm.me>
2025-07-01 21:27:21 +0100

    refactor: start again with less dependencies

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,1 @@
-// Import our custom CSS
-import '../scss/styles.scss';
-
-// Import only the Bootstrap components we need
-// import { Popover } from 'bootstrap';
-
-// // Create an example popover
-// document.querySelectorAll('[data-bs-toggle="popover"]')
-//   .forEach(popover => {
-//     new Popover(popover)
-//   });
+import '../styles/style.css';
