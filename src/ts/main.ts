26f1f2629a5e10fd47b3e426807ed1e18630b886
Dan <Daniel-J-Price.dev@pm.me>
2025-07-04 03:53:44 +0100

    refactor: change application flow to make use of svelte

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,9 @@
-import '../styles/style.css';
+import { mount } from 'svelte'
+import App from '../App.svelte'
+import '../styles/style.css'
+
+const app = mount(App, {
+  target: document.getElementById('app')!,
+})
+
+export default app
